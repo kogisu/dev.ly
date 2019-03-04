508577c80892243e78847699c34f8ce9957369ca
Kent Ogisu <kentogisu@gmail.com>
2019-03-03 20:32:17 -0500

    Removed comments

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,14 +10,13 @@ opts.secretOrKey = config.SECRET;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, async (jwt_payload, done) => {
-      // const queryStr =  `select * from users where id=${jwt_payload.id}`;
       try {
         const user = await User.findAll({
           where: {
             id: jwt_payload.id
           }
         });
-        // const user = await db.queryDB(queryStr, []);
+
         if (user.length) {
           return done(null, user[0]);
         }
